f542b8dd127cea3e489cea45c26ba9e363476c2f
Brett Rogers <bdrogers@fullsail.edu>
2017-09-20 21:33:32 -0400

    Changed the skill circles from an SVG to being made with CSS. Hit a stopping point. All information implemented along with form from Wufoo.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const projects = {
   imageGallery: {
-    img: './images/rollercoaster-small.png',
+    img: './images/rollercoaster.png',
     alt: 'A screenshot depicting the the web page with images of rollercoasters.',
     title: 'Responsive Image Gallery',
     description: 'This project is a small image gallery utlizing the Unsplash API and CSS animations. The images were brought in from Unsplash and have a hover effect that displays metadata with an animation. Along with this, the use of a basic animation was used on the logo to spin when clicked.',
     websiteLink: 'http://krogers78.github.io/responsiveImagePage'
   },
   gauntlet: {
-    img: './images/gauntlet-small_1.png',
+    img: './images/gauntlet.png',
     alt: 'A screenshot depicting the the web page showing the character creation form.',
     title: 'The Gauntlet',
     description: 'This project takes the user through a character creation form that consists of multiple options. A name will be chosen along with one of three different character classes that each have weapons specific to the class. After selecting the class and weapon; the user can add armor to their character and begin the game. Once in the game the user will randomally encounter four different enemies that each have different stats and can land critical hit and miss their attack all through the use of the Random class.',
@@ -56,7 +56,7 @@ if (theGauntlet) {
 
 function CreateTheString(image, theAlt, theTitle, theDescription, theLink) {
   let htmlString = `<img class="img-fluid" src="${image}" alt="${theAlt}" />
-                    <h2>${theTitle}</h2>
+                    <h2 class="pt-3">${theTitle}</h2>
                     <p>${theDescription}</p>
                     <p><a class="btn btn-primary" href="${theLink}">View the Live Website</a></p>`
   return htmlString
